3cea8d3ac9bc3c391dc289f2c4aa9c0a43875a5f
Keenan Yang <keenan.yang1027@gmail.com>
2025-09-12 22:37:59 -0400

    removed pfp

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Experiences from '../components/Experiences';
 import QuickLinks from '../components/QuickLinks';
 import ProjectsSection from '../components/ProjectSection';
 
+
 export default function HomePage() {
   const copyEmail = () => {
     navigator.clipboard.writeText('keenanyang1027@gmail.com');
@@ -13,21 +14,18 @@ export default function HomePage() {
   };
 
   return (
-    <div className="min-h-screen bg-[#f8f8f8]">
-      {/* Header (not centered) */}
-      <Header />
-
+    <div className="min-h-screen bg-[#f8f8f8] mt-36">
       {/* Main Content Wrapper */}
       <div className="w-full max-w-7xl mx-auto px-6 mt-24">
         {/* Hero Section */}
-        <div className="text-center">
-          <h1 className="text-4xl sm:text-6xl font-bold">Hey, I’m Keenan Yang!</h1>
+        <div className="text-center ">
+          <h1 className="text-4xl sm:text-6xl font-bold hover:scale-[1.05] transition-all duration-300 cursor-pointer">Hey, I’m Keenan Yang!</h1>
 
-          <div className="gap-2 mt-6 text-2xl sm:text-4xl">
+          <div className="gap-2 mt-6 text-2xl sm:text-4xl hover:scale-[1.05] transition-all duration-300 cursor-pointer">
             <span className="font-medium text-[#a9a9a9] mr-2">I am a</span>
             <span className="font-semibold mr-2">Computer Science Student</span>
             <span className="font-medium text-[#a9a9a9] mr-2">at</span>
-            <span className="font-bold bg-gradient-to-r from-[#004292] via-[#b90e32] to-[#fabc0f] [background-clip:text] [-webkit-background-clip:text] text-transparent [-webkit-text-fill-color:transparent]">
+            <span className="font-bold bg-gradient-to-r from-[#004292] via-[#b90e32] to-[#fabc0f] [background-clip:text] [-webkit-background-clip:text] text-transparent [-webkit-text-fill-color:transparent] ">
               Queen’s University
             </span>
           </div>
@@ -45,7 +43,7 @@ export default function HomePage() {
         </div>
 
         {/* Projects Section */}
-        <div className="w-full flex flex-col items-center mt-32 px-4">
+        <div className="w-full flex flex-col items-center mt-32 px-4 ">
           <h1 className="text-5xl font-bold text-center">Projects</h1>
           <p className="mt-6 text-3xl font-medium text-[#a9a9a9] break-words max-w-[1000px] text-center">
             Check out some of my work! — This Summer my goal was to build as many 
